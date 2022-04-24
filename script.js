f7ede7a45ac04814b3568fbbdb8e573ceff6a191
Jazz Whitfield <jazzwhitfield123@gmail.com>
2022-04-24 18:38:06 -0400

    Link stylesheet to index. Create a 16x16 grid of squares. Change for loop to create 256 squares to fill the grid.

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const grid = document.querySelector("#grid");
+// const grid = document.querySelector("#grid");
 
-for (x = 0; x < 16; x++) {
+for (x = 0; x < 256; x++) {
   const content = document.createElement("div");
-
-  content.classList.add = "square";
-
-  content.textContent = "hello world";
 
   grid.appendChild(content);
 }
