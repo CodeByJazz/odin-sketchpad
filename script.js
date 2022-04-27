ac4516314f986e746ef758a212b8773cec03473a
Jazz Whitfield <jazzwhitfield123@gmail.com>
2022-04-27 12:33:06 -0400

    Create two buttons with separate id's. Create a variable that selects each element within the main container.

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-// const grid = document.querySelector("#grid");
+const grid = document.querySelector("#grid");
+const start = document.querySelector("#start-button");
+const reset = document.querySelector("#reset-button");
+const maxWidth = 400;
 
 for (x = 0; x < 256; x++) {
   const content = document.createElement("div");
 
   grid.appendChild(content);
 }
+
+// function newGrid(number) {
+//   //   window.location.reload();
+//   let gridNum = prompt("How many sqaures per side?");
+//   alert(gridNum);
+//   let squares = gridNum * gridNum;
+//   for (x = 0; x < squares; x++) {
+//     const content = document.createElement("div");
+//     grid.appendChild(content);
+//   }
+// }
