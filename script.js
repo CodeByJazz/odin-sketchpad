b3da76f65910ac19eb8e2baad16997d0e9c0c0ab
Jazz Whitfield <jazzwhitfield123@gmail.com>
2022-04-27 12:50:08 -0400

    Create a function that makes a default grid when the page is loaded. Append the grid to the grid container div.

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ const start = document.querySelector("#start-button");
 const reset = document.querySelector("#reset-button");
 const maxWidth = 400;
 
-for (x = 0; x < 256; x++) {
-  const content = document.createElement("div");
+window.addEventListener("load", makeGrid);
 
-  grid.appendChild(content);
+let size = 16;
+
+function makeGrid() {
+  for (let r = 0; r < size; r++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let c = 0; c < size; c++) {
+      let column = document.createElement("div");
+      column.classList.add("column");
+      row.appendChild(column);
+    }
+    document.getElementById("grid").appendChild(row);
+  }
 }
-
 // function newGrid(number) {
 //   //   window.location.reload();
 //   let gridNum = prompt("How many sqaures per side?");
